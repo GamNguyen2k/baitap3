ae108ec5f20c22d674e03ea55cdc60de5208d102
Nguyen Gam <gamb1812792@student.ctu.edu.vn>
2021-05-04 23:13:34 +0700

    Modify baitap3.py

--- baitap3/baitap3.py
+++ baitap3/baitap3.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 
 print("Bai tap 3")
+print("Install VS Code and Github Pull Requests-Issues extension")
+print("=========================================================")
+print("Da Xong")
